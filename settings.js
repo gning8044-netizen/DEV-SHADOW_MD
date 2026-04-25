9b6c8d7dc4851151d659b83a2a34e86f3e59f48e
Shadow prime <gning8044@gmail.com>
2026-04-25 19:17:08 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃',
+  author: '‎',
+  botName: "𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃",
+  botOwner: 'PAPII', // Your name
+  ownerNumber: '221769325203', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.9",
+  updateZipUrl: "https://github.com/Dev_Shadow_tech/DEVSHADOW-MD/archive/refs/heads/main.zip'",
+};
+
+module.exports = settings;
